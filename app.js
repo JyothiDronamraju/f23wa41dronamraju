3fe60d8df668a430d7b19d99ca8e4c5aa242b360
JyothiDronamraju <143140898+JyothiDronamraju@users.noreply.github.com>
2023-10-25 15:03:50 -0500

    added bonus to app.js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
-var computationRouter = require('./routes/computation');
 var app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -15,21 +15,59 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Register your route handlers
 app.use('/mydata', mydataRouter);
 app.use('/users', usersRouter);
-app.use('/computation', computationRouter);
 
-// catch 404 and forward to error handler
+// Define a new route handler for /computation
+app.get('/computation', (req, res) => {
+  // Extract the last digit from your ID (e.g., 'dronamraju' from your full name)
+  const id = 'dronamraju';
+  const lastDigit = id[id.length - 1];
+
+  // Parse the query parameter 'x' (if it exists)
+  const x = parseFloat(req.query.x);
+
+  // Use the last digit to determine the Math function
+  let result;
+  switch (lastDigit % 4) {
+    case 0:
+      result = Math.log(x || Math.random());
+      break;
+    case 1:
+      result = Math.log10(x || Math.random());
+      break;
+    case 2:
+      result = Math.exp(x || Math.random());
+      break;
+    case 3:
+      result = Math.sqrt(x || Math.random());
+      break;
+    default:
+      result = 'Invalid last digit';
+  }
+
+  // Construct the response string
+  const response = `[Math.log()] applied to ${x || 'a random value'} is ${result}`;
+
+  // Send the response
+  res.send(response);
+});
+
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-// error handler
+
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
 module.exports = app;
