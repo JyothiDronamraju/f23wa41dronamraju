c7de4a055ee9548960294e78a896b3e8003c0ebc
JyothiDronamraju <143140898+JyothiDronamraju@users.noreply.github.com>
2023-10-25 13:41:05 -0500

    added users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+// Initialize sum and more
+let sum = 0;
+let more = 0;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // Increment more and add it to sum
+  more += 1;
+  sum += more;
+
+  // Send the response with the updated sum
+  res.send(`Sum is: ${sum}`);
 });
 
 module.exports = router;
